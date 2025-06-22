3160fec963c1a3dfb3a6d5d3267c548ff3075137
Sven Jacobs <github@svenjacobs.com>
2025-06-22 18:36:13 +0200

    fix: increment version code manually b/c of deployment problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,8 +43,8 @@ android {
 		applicationId = "com.svenjacobs.app.leon"
 		minSdk = Android.minSdk
 		targetSdk = Android.targetSdk
-		versionCode = 270
-		versionName = "49"
+		versionCode = 271
+		versionName = "50"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
