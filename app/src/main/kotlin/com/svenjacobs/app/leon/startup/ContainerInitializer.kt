beb43a4bd4f29c38b1b9cb528a2becab1a057eff
Janusz Kowalczyk <kowalczykjanusz@gmail.com>
2026-03-15 14:17:12 +0000

    add sanitizers for autotrader.co.uk and cargurus.co.uk (#674)
    * add sanitizers for autotrader.co.uk and cargurus.co.uk
    
    * fix ordering of sanitizers
    
    * fix ordering of sanitizer imports and resource strings
    
    * fix linting errors
    
    * rm offending newline

--- app/src/main/kotlin/com/svenjacobs/app/leon/startup/ContainerInitializer.kt
+++ app/src/main/kotlin/com/svenjacobs/app/leon/startup/ContainerInitializer.kt
@@ -25,6 +25,8 @@ import com.svenjacobs.app.leon.core.domain.sanitizer.amazon.AmazonProductSanitiz
 import com.svenjacobs.app.leon.core.domain.sanitizer.amazon.AmazonSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.aol.AolSearchSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.at.AtAnalyticsSanitizer
+import com.svenjacobs.app.leon.core.domain.sanitizer.autotrader.AutoTraderSanitizer
+import com.svenjacobs.app.leon.core.domain.sanitizer.cargurus.CarGurusSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.change.ChangeSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.cx.CxAnalyticsSanitizer
 import com.svenjacobs.app.leon.core.domain.sanitizer.ebay.EbaySanitizer
@@ -88,6 +90,8 @@ class ContainerInitializer : DistinctInitializer<Unit> {
 				AmazonSanitizer(),
 				AolSearchSanitizer(),
 				AtAnalyticsSanitizer(),
+				AutoTraderSanitizer(),
+				CarGurusSanitizer(),
 				ChangeSanitizer(),
 				CxAnalyticsSanitizer(),
 				EbaySanitizer(),
