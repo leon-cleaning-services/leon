f9a98bd81daa231c345978f3c13d31a22a037a48
Sven Jacobs <github@svenjacobs.com>
2026-09-05 17:53:01 +0200

    feat(sanitizer): add AvantLink sanitizer (#818)
    AvantLink referral links hide the affiliate target, percent-encoded,
    in the "url" parameter of /click.php. This sanitizer extracts it.
    
    Closes #608

---
+++ core-domain/src/main/kotlin/com/svenjacobs/app/leon/core/domain/sanitizer/catalog/AvantLink.kt
@@ -0,0 +1,41 @@
+/*
+ * Léon - The URL Cleaner
+ * Copyright (C) 2026 Sven Jacobs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.svenjacobs.app.leon.core.domain.sanitizer.catalog
+
+import com.svenjacobs.app.leon.core.domain.sanitizer.Decode
+import com.svenjacobs.app.leon.core.domain.sanitizer.HostMatch
+import com.svenjacobs.app.leon.core.domain.sanitizer.Match
+import com.svenjacobs.app.leon.core.domain.sanitizer.Rule
+import com.svenjacobs.app.leon.core.domain.sanitizer.Sanitizer
+import com.svenjacobs.app.leon.core.domain.sanitizer.SanitizerId
+import com.svenjacobs.app.leon.core.domain.sanitizer.Source
+import kotlinx.collections.immutable.persistentListOf
+
+val AvantLink =
+    Sanitizer(
+        id = SanitizerId("avantlink"),
+        name = "AvantLink",
+        // The affiliate target is hidden, percent-encoded, in the "url" parameter of the referral
+        // link: /click.php?...&url=<target>
+        rules =
+            persistentListOf(
+                Rule.Follow(Source.Parameter("url"), persistentListOf(Decode.PercentDecode))
+            ),
+        match =
+            persistentListOf(Match(HostMatch.Domain("avantlink.com"), pathPrefix = "/click.php")),
+    )
